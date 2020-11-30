7844f8c5aaf92e9b5d725016585d62d3ad013be5
Bibek Ghimire <bibekgh@gmail.com>
2020-11-29 19:09:55 -0600

    finished code with integor and negative number checking

--- src/Sqrt.java
+++ src/Sqrt.java
@@ -1,31 +1,47 @@
 //Need below to create dialog boxes
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 //Need below to do math operations
-import java.lang.Math;
 //Start of Sqrt function 
 public class Sqrt {
     //Main function
     public static void main(String[] args) {
     //Get input from user through this dialog
-        String input1string =
-                JOptionPane.showInputDialog(null, 
-                "Enter your number", 
-                "Taking the square root of a number!", 
-                JOptionPane.QUESTION_MESSAGE);
+    Scanner scan = new Scanner(System.in);   
+    System.out.println("Please enter the positive integer to take the square root of.");
+    double userinput = scan.nextDouble(); 
+    //need something to ensure that number is positive and an integer 
+    do {
 
-    Double input1 = Double.parseDouble(input1string);
-    //Math Operation - stored into result 1 variable
-    Double result1 = Math.sqrt(input1);
-    //Convert to whole number 
-    int round1= (int)Math.round(result1);
-    //Print result into system
-                JOptionPane.showMessageDialog(null, 
-                "The square root of " + input1 + " is " + round1, 
-                "Taking the square root of a number!", 
-                JOptionPane.INFORMATION_MESSAGE);      
-    }
-//trying again to see if dev branch will push
-Double input2
+        System.out.print("Please enter a positive integer number: "); 
+        if (scan.hasNextInt()) {
+            userinput = scan.nextInt();
+
+        }
+        else {
+            System.out.println("I need an int, please try again.");
+            scan.nextLine(); 
+        }
+    } while (userinput <=0);
+        double num=userinput; 
+        double temp; 
+        double sr=num/2; 
+       
+        //Algo for finding square root
+        do{
+            temp=sr; 
+            sr=(temp+(num/temp))/2;
+        }
+        while ((temp-sr)!=0); 
+        
+        //Converting to whole number
+        double add = sr+0.5; 
+        int result = (int)add;
+
+        //System output
+        System.out.println("The Square rooot of " + userinput +" is " + result + " rounded to the nearest whole number.");   
+        scan.close(); 
+        } 
 
 }
+
 
